06ce73ee530039caba4a606aeac59fde93c76fcc
Alexandru Sorin Racz <alexandrusorin.racz@alu.uclm.es>
2025-10-07 17:19:50 +0200

    Primeros cambios. Llegado a punto 2.2 sin probar.

---
+++ cliente/modelo.js
@@ -0,0 +1,12 @@
+function Sistema(){
+    this.usuarios={};
+    this.agregarUsuario=function(nick){
+    this.usuarios[nick]=new Usuario(nick);
+    }
+    this.obtenerUsuarios=function(){
+        return this.usuarios;
+    }
+}
+function Usuario(nick){
+    this.nick=nick;
+}
